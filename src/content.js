f57ef7560a20ea71349622386aeda6f3abd3caf9
prajith <rprajith7@gmail.com>
2023-08-01 11:00:35 +0530

    today work

---
+++ src/content.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Content = () => {
+  return (
+    <div className='contentDiv'>
+        <h1>Opening Party</h1>
+        <p>Written by Prajith</p>
+        <h1>Welcome to the New Blog</h1>
+        <p>Written by kv</p>
+    </div>
+  )
+}
+
+export default Content
---
+++ src/navbar.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <>
+    <div id="nav-bar">
+        <h1>The Dojo Blog</h1>
+        <div>
+        <button className='homeBtn'>Home</button>
+         <button className='titleBtn'>New Blog</button>
+        </div>
+  </div>
+  <hr/>
+    </>
+  
+  )
+}
+export default Navbar
